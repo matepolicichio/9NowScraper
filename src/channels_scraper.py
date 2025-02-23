5a0578d528e4056fac1149c3dcf078a7bc1d8f03
mateo <mateopolicichio@gmail.com>
2025-02-23 12:41:44 -0300

    Update

--- src/channels_scraper.py
+++ src/channels_scraper.py
@@ -237,6 +237,8 @@ try:
                         try:
                             # Ver el detalle del programa
                             program_link = program.find_element(By.CSS_SELECTOR, "a")
+                            driver.execute_script("arguments[0].scrollIntoView(true);", program_link)  # Scroll hacia el programa
+                            WebDriverWait(driver, 5).until(EC.element_to_be_clickable(program_link))
                             program_link.click()
 
                             # Extraer el título del programa
