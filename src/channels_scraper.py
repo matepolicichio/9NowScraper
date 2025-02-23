121c3acf3fbda49ecad264dca245be28470a2db9
mateo <mateopolicichio@gmail.com>
2025-02-23 17:14:53 -0300

    Update

--- src/channels_scraper.py
+++ src/channels_scraper.py
@@ -187,38 +187,38 @@ try:
 
     print("✅ Página cargada correctamente.")
 
-    # Esperar que la lista de canales cargue
-    WebDriverWait(driver, 15).until(
-        EC.presence_of_element_located((By.CSS_SELECTOR, ".guide__grid"))
-    )
+    # # Esperar que la lista de canales cargue
+    # WebDriverWait(driver, 15).until(
+    #     EC.presence_of_element_located((By.CSS_SELECTOR, ".guide__grid"))
+    # )
 
-    # Obtener la lista de días de navegación
-    day_nav_list = WebDriverWait(driver, 10).until(
-        EC.presence_of_all_elements_located((By.CSS_SELECTOR, ".day-nav__list__item"))
-    )
-    print(f"🔹 Días de navegación extraídos: {len(day_nav_list)}")
+    # # Obtener la lista de días de navegación
+    # day_nav_list = WebDriverWait(driver, 10).until(
+    #     EC.presence_of_all_elements_located((By.CSS_SELECTOR, ".day-nav__list__item"))
+    # )
+    # print(f"🔹 Días de navegación extraídos: {len(day_nav_list)}")
 
-    # Lista para almacenar los datos de todos los canales
-    channels_data = []
+    # # Lista para almacenar los datos de todos los canales
+    # channels_data = []
     
-    # Recorrer cada canal
-    for day_nav in day_nav_list:        
-        try:
-            print("\n🔄 Procesando un nuevo día...")
+    # # Recorrer cada canal
+    # for day_nav in day_nav_list:        
+    #     try:
+    #         print("\n🔄 Procesando un nuevo día...")
 
-            # Subir al inicio de la página
-            driver.execute_script("window.scrollTo(0, 0);")
+    #         # Subir al inicio de la página
+    #         driver.execute_script("window.scrollTo(0, 0);")
 
-            # Extraer el enlace del día
-            day_nav_link = day_nav.find_element(By.CSS_SELECTOR, "a")
+    #         # Extraer el enlace del día
+    #         day_nav_link = day_nav.find_element(By.CSS_SELECTOR, "a")
 
-            # Obtener la fecha de cada day_nav
-            day_nav_date = day_nav_link.get_attribute("data-date")
-            day_nav_link.click()
-            print(f"\n\n✅ Click en el día {day_nav_date}.\nURL actual: {driver.current_url}")
+    #         # Obtener la fecha de cada day_nav
+    #         day_nav_date = day_nav_link.get_attribute("data-date")
+    #         day_nav_link.click()
+    #         print(f"\n\n✅ Click en el día {day_nav_date}.\nURL actual: {driver.current_url}")
             
-            # Esperar que la grilla de programas cargue
-            WebDriverWait(driver, 15).until(EC.presence_of_element_located((By.CSS_SELECTOR, ".footer")))
+    #         # Esperar que la grilla de programas cargue
+    #         WebDriverWait(driver, 15).until(EC.presence_of_element_located((By.CSS_SELECTOR, ".footer")))
             
         #     # Obtener la filas de la grilla (canales menos el ON DEMAND)
         #     guide_rows = WebDriverWait(driver, 10).until(
@@ -303,12 +303,12 @@ try:
         #             print("⚠️ Advertencia: No se encontró información del canal.")
         #             continue
 
-        except TimeoutException:
-            print("⚠️ No se pudo extraer la información del canal a tiempo.")
+#         except TimeoutException:
+#             print("⚠️ No se pudo extraer la información del canal a tiempo.")
 
-        except Exception as e:
-            print(f"Error procesando el canal: {e}")
-            print(traceback.format_exc())
+#         except Exception as e:
+#             print(f"Error procesando el canal: {e}")
+#             print(traceback.format_exc())
 
 except WebDriverException as e:
     print(f"❌ Error al iniciar WebDriver: {e}")
